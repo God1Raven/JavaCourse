f7e9554a5ba7e648d7a68809970905a480b6df70
God1Raven <Noymov@mail.ru>
2024-03-10 15:19:06 +0100

    Create project

---
+++ src/entity/Person.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Person {
+    int id;
+    String name;
+    String lastName;
+    String email;
+    public Person(int id, String name, String lastName, String email){
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
